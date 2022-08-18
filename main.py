89e9814a88f201d385a19c5bc2ecaecbc52db312
opliyal3 <dakara880@gmail.com>
2022-08-19 00:47:04 +0800

    feat: bot

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import discord
+from discord.ext import commands
+import json
 
-client = discord.Client()
+with open('setting.json', 'r', encoding='utf-8') as jfile:
+    jdata = json.load(jfile)
+bot = commands.Bot(command_prefix='')
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
 
-@client.event
-async def echo(message):
-    if message.author == client.user:
-        return
-
-    if message.content:
-        await message.channel.send(message.content)
+@bot.command()
+async def echo(ctx, *, message):
+    await ctx.send(f'{message}')
 
 
-client.run()
+@bot.command()
+async def create(ctx, *, name=None):
+    guild = ctx.message.guild
+    if name is None:
+        await ctx.send('Sorry, but you have to insert a name. Try again, but do it like this: `>create [channel name]`')
+    else:
+        await guild.create_text_channel(name)
+        await ctx.send(f"Created a channel named {name}")
+
+
+@bot.command()
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'User {member} has been kicked from the server.')
+
+
+@bot.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+
+bot.run(jdata['TOKEN'])
